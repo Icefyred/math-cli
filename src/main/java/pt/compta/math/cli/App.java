1a144c0d6cea0c79d551c130f808dae5bde34816
Icefyred <dcastilho001@hotmail.com>
2019-03-30 14:19:34 +0000

    Changed the content of the App.java file

--- src/main/java/pt/compta/math/cli/App.java
+++ src/main/java/pt/compta/math/cli/App.java
@@ -1,11 +1,26 @@
 package pt.compta.math.cli;
 
-/**
- * Hello world!
- *
- */
+import pt.compta.tools.fourthmaven.MathUtils;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//sum args[0], firstNumber args[1], secondNumber args[2]
+		int firstNumber, secondNumber;
+		
+		firstNumber = Integer.parseInt(args[1]);
+		secondNumber = Integer.parseInt(args[2]);
+		String typeOfOperation = args[0];
+		
+		String resultFromOpeartions = "Result: ";
+		
+		if(typeOfOperation.equals("sum")) {
+			resultFromOpeartions += MathUtils.sumOfTwoNumbers(firstNumber, secondNumber);
+		}
+		else {
+			resultFromOpeartions += MathUtils.subtractOfTwoNumbers(firstNumber, secondNumber);
+		}
+		
+		System.out.println(resultFromOpeartions);
+		
 	}
 }
